b397371ace653708cd06aa93487c253e5caa6b08
Jeison <jeisonortiz1516@gmail.com>
2024-03-13 20:03:12 -0500

    add temp and permanent employee

--- src/registerEmployees/App.java
+++ src/registerEmployees/App.java
@@ -7,8 +7,10 @@ public class App
         GestionEmpleados empleados = new GestionEmpleados();
 
         Empleado empleado = new EmpleadoPermanente("jeison",29,1,10000000);
+        Empleado empleado2 = new EmpleadoTemporal("daniela",28,2,20000000);
 
         empleados.agregarEmpleado(empleado);
+        empleados.agregarEmpleado(empleado2);
 
         empleados.mostrarEmpleados();
 
